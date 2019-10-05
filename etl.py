06432cece4a90a31c6e5c906a4aa493a72275d98
triplewy <triplewy@gmail.com>
2019-10-05 23:49:25 +0000

    Linted

--- etl.py
+++ etl.py
@@ -2,6 +2,7 @@ import csv
 from time import sleep
 from pymongo import MongoClient
 
+
 def loadAirbnb():
     arr = []
     with open('AB_NYC_2019.csv', newline='') as csvfile:
@@ -12,6 +13,7 @@ def loadAirbnb():
     inserted_ids = db.airbnb.insert_many(arr).inserted_ids
     return len(inserted_ids)
 
+
 def loadTaxi():
     arr = []
     with open('TAXI_NYC_2019.csv', newline='') as csvfile:
@@ -21,6 +23,7 @@ def loadTaxi():
 
     inserted_ids = db.taxi.insert_many(arr).inserted_ids
     return len(inserted_ids)
+
 
 if __name__ == "__main__":
     db = MongoClient().test
--- query.py
+++ query.py
@@ -5,15 +5,23 @@ db = MongoClient().test
 
 '''
     AirBNB Schema:
-    _id, id, name, host_id, host_name, neighbourhood_group, neighbourhood, latitude, longitude, room_type, price, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365
-    {'_id': ObjectId('5d992183589c75947f14835e'), 'id': '36444734', 'name': 'Sunny Upper West Side Apt. 3mins from Central Park', 'host_id': '32987938', 'host_name': 'Alex', 'neighbourhood_group': 'Manhattan', 'neighbourhood': 'Upper West Side', 'latitude': '40.77889', 'longitude': '-73.97668', 'room_type': 'Entire home/apt', 'price': '143', 'minimum_nights': '3', 'number_of_reviews': '0', 'last_review': '', 'reviews_per_month': '', 'calculated_host_listings_count': '1', 'availability_365': '10'}
+    id, name, host_id, host_name, neighbourhood_group, neighbourhood, latitude, longitude, room_type, price, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365
+    2539,Clean & quiet apt home by the park,2787,John,Brooklyn,Kensington,40.64749,-73.97237,Private room,149,1,9,2018-10-19,0.21,6,365
+
+
+    Taxi Schema:
+    key, pickup_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count
+    2011-10-08 11:53:44.0000002,2011-10-08 11:53:44 UTC,-73.982524,40.75126,-73.979654,40.746139,1
 '''
+
 
 def query1():
     result = db.airbnb.find_one()
 
+
 def query2():
-    docs = db.airbnb.find({ 'neighbourhood' : 'Upper West Side' }, { '_id': 0, 'latitude': 1, 'longitude': 1, 'price': 1 })
+    docs = db.airbnb.find({'neighbourhood': 'Upper West Side'}, {
+                          '_id': 0, 'latitude': 1, 'longitude': 1, 'price': 1})
 
     with open('query2.csv', 'w', newline='') as csvfile:
         fieldnames = ['latitude', 'longitude', 'price']
@@ -22,14 +30,18 @@ def query2():
         for doc in docs:
             writer.writerow(doc)
 
+
 def query3():
     pass
+
 
 def query4():
     pass
 
+
 def query5():
     pass
+
 
 if __name__ == "__main__":
     query2()
